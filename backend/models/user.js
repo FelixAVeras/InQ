0eea03e8e649df6666a9fde1ccafe63f13f38c77
felixaveras <fcarvajal44@gmail.com>
2025-03-25 12:41:51 -0400

    working on auth

---
+++ backend/models/user.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['client', 'professional'], // Define los roles permitidos
+        required: true,
+        default: 'client' // Establece un rol por defecto
+    },
+    // Otros campos comunes para todos los usuarios (opcional)
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
